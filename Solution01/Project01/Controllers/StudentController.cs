52fb44b3c7f02220934ef54949e733a969f3cf9c
Oybek <61838041+s17712@users.noreply.github.com>
2020-04-19 14:43:06 +0200

    initial change

---
+++ Solution01/Project01/Controllers/StudentController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Project01.Controllers
+{
+    [Route("api/students")]
+    [ApiController]
+    public class StudentController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult GetStudents() 
+        {
+
+            return Ok();
+        }
+    }
+}
