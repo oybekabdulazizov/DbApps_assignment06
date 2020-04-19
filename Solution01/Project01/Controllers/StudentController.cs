e0c187ade6579f6293a11ed070f4ab4574f146ba
Oybek <61838041+s17712@users.noreply.github.com>
2020-04-19 15:07:37 +0200

    just basic operations performed

--- Solution01/Project01/Controllers/StudentController.cs
+++ Solution01/Project01/Controllers/StudentController.cs
@@ -1,9 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Project01.Helpers;
+using Project01.Models;
 
 namespace Project01.Controllers
 {
@@ -14,8 +17,33 @@ namespace Project01.Controllers
         [HttpGet]
         public IActionResult GetStudents() 
         {
+            var students = new List<Student>();
+            using (var connection = new SqlConnection(DbServer.localConnection)) 
+            {
+                using (var command = new SqlCommand()) 
+                {
+                    command.Connection = connection;
+                    command.CommandText = @"SELECT IndexNumber, FirstName, LastName, BirthDate, IdENrollment FROM Student;";
+                    connection.Open();
 
-            return Ok();
+                    using var dr = command.ExecuteReader();
+
+                    while (dr.Read()) 
+                    {
+                        var student = new Student
+                        {
+                            IndexNumber = dr["IndexNumber"].ToString(),
+                            FirstName = dr["FirstName"].ToString(),
+                            LastName = dr["LastName"].ToString(),
+                            BirthDate = DateTime.Parse(dr["BirthDate"].ToString()),
+                            IdEnrollment = int.Parse(dr["IdEnrollment"].ToString())
+                        };
+                        students.Add(student);
+                    }
+
+                    return Ok(students);
+                }
+            }
         }
     }
 }
---
+++ Solution01/Project01/Helpers/DbServer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Project01.Helpers
+{
+    public class DbServer
+    {
+        public static string localConnection { get; } = @"Data Source=(LocalDb)\LocalDatabase;Initial Catalog=semester4_dotnet;Integrated Security=True";
+    }
+}
